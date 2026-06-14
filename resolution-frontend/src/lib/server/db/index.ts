29dd1c7e8c0c11c5011625867fa4a58e1f7ef9b1
Mahad Kalam <mahadkalam1234@gmail.com>
2026-06-14 16:14:22 +0100

    I will pool this club

--- resolution-frontend/src/lib/server/db/index.ts
+++ resolution-frontend/src/lib/server/db/index.ts
@@ -1,10 +1,17 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
-import { env } from '$env/dynamic/private';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import { env } from "$env/dynamic/private";
+import * as schema from "./schema";
 
 const pool = new pg.Pool({
-  connectionString: env.DATABASE_URL
+  connectionString: env.DATABASE_URL,
+  idleTimeoutMillis: 0,
+  keepAlive: true,
+  max: 5,
 });
+
+pool
+  .query("SELECT 1")
+  .catch((err) => console.error("[db] prewarm failed", err));
 
 export const db = drizzle(pool, { schema });
